a6f58bf6d4c33bf3f676ae0c40f6b2d612214732
Esi're <esiri92@gmail.com>
2022-08-14 16:40:35 +0100

    chore: update tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EF2C5A',
+        secondary: '#FFFBF7',
+      },
+
+      fontFamily: {
+        sans: ['General Sans', ...defaultTheme.fontFamily.sans],
+        'sans-alt': ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+
+  plugins: [
+    plugin(({ addVariant }) => {
+      ['compact'].forEach(variant => {
+        addVariant(variant, [`&.${variant}`, `:merge(.${variant}), &`]);
+      });
+    }),
+  ],
 };
