a9146ec384ce57cc2f6d2d8be6b2d6f6f9dba9ac
Esi're <esiri92@gmail.com>
2022-08-14 19:15:00 +0100

    chore: add tailwindcss scrollbar plugin.

---
+++ src/pages/transactions.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Transactions: NextPage = () => {
+  return (<div></div>);
+};
+
+export default Transactions;
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,5 +24,34 @@ module.exports = {
         addVariant(variant, [`&.${variant}`, `:merge(.${variant}), &`]);
       });
     }),
+    plugin(({ addBase, theme }) => {
+      addBase({
+        '*': {
+          scrollbarWidth: theme('scrollbar.moz-width', 'thin'),
+          scrollbarColor: `${theme('scrollbar.thumb-color', 'lightgray')} ${theme(
+            'scrollbar.track-color',
+            '#FFF',
+          )}`,
+        },
+
+        '::-webkit-scrollbar': {
+          width: theme('scrollbar.width', '12px'),
+          height: theme('scrollbar.height', '12px'),
+        },
+
+        '::-webkit-scrollbar-thumb': {
+          backgroundColor: theme('scrollbar.thumb-color', 'lightgray'),
+          borderRadius: theme('scrollbar.radius', '12px'),
+          border: `3px solid ${theme('scrollbar.track-color', '#FFF')}`,
+          '&:hover': {
+            backgroundColor: theme('scrollbar.hover.thumb-color', 'gray'),
+          },
+        },
+
+        '::-webkit-scrollbar-track': {
+          background: theme('scrollbar.track-color', '#FFF'),
+        },
+      });
+    }),
   ],
 };
